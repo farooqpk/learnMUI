68e63cd8c680bb5b0a98b6d2f1db1940a3e41802
Ummar Farooq <farooqcherur@gmail.com>
2023-07-18 23:53:29 +0530

    implimented pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,22 @@
 import { Stack, colors, createTheme, ThemeProvider } from "@mui/material";
-import { ButtonMUI } from "./components/buttonMUI";
-import { TypographyMUI } from "./components/typographyMUI";
-import { TextFieldMUI } from "./components/textFieldMUI";
-import { SelectMUI } from "./components/selectMUI";
-import { RadioMUI } from "./components/radioMUI";
-import { CheckboxMUI } from "./components/checkboxMUI";
-import { SwichMUI } from "./components/swichMUI";
-import { RatingMUI } from "./components/ratingMUI";
-import { AutoCompleteMUI } from "./components/autoCompleteMUI";
-import { LayoutMUI } from "./components/layoutMUI";
-import { CardMUI } from "./components/cardMUI";
-import { AccordianMUI } from "./components/accordianMUI";
-import { MUIresponsiveness } from "./components/MUIresponsiveness";
-import { NavbarMUI } from "./components/navbarMUI";
-import { TableMUI } from "./components/tableMUI";
-import { ListMUI } from "./components/listMUI";
+// import { ButtonMUI } from "./components/buttonMUI";
+// import { TypographyMUI } from "./components/typographyMUI";
+// import { TextFieldMUI } from "./components/textFieldMUI";
+// import { SelectMUI } from "./components/selectMUI";
+// import { RadioMUI } from "./components/radioMUI";
+// import { CheckboxMUI } from "./components/checkboxMUI";
+// import { SwichMUI } from "./components/swichMUI";
+// import { RatingMUI } from "./components/ratingMUI";
+// import { AutoCompleteMUI } from "./components/autoCompleteMUI";
+// import { LayoutMUI } from "./components/layoutMUI";
+// import { CardMUI } from "./components/cardMUI";
+// import { AccordianMUI } from "./components/accordianMUI";
+// import { MUIresponsiveness } from "./components/MUIresponsiveness";
+// import { NavbarMUI } from "./components/navbarMUI";
+// import { TableMUI } from "./components/tableMUI";
+// import { ListMUI } from "./components/listMUI";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { PaginationMUI } from "./components/paginationMUI";
 
 const theme = createTheme({
   palette: {
@@ -28,28 +30,37 @@ const theme = createTheme({
 });
 
 function App() {
+  const client = new QueryClient();
+
   return (
     <>
-      <ThemeProvider theme={theme}>
-        <Stack gap={7}>
-          <NavbarMUI />
-          <TypographyMUI />
-          <ButtonMUI />
-          <TextFieldMUI />
-          <SelectMUI />
-          <RadioMUI />
-          <CheckboxMUI />
-          <SwichMUI />
-          <RatingMUI />
-          <AutoCompleteMUI />
-          <LayoutMUI />
-          <CardMUI />
-          <AccordianMUI />
-          <MUIresponsiveness />
-          <TableMUI/>
-          <ListMUI/>
-        </Stack>
-      </ThemeProvider>
+      <QueryClientProvider client={client}>
+        <ThemeProvider theme={theme}>
+          
+          <Stack>
+            <PaginationMUI />
+          </Stack>
+
+          {/* <Stack gap={7}>
+            <NavbarMUI />
+            <TypographyMUI />
+            <ButtonMUI />
+            <TextFieldMUI />
+            <SelectMUI />
+            <RadioMUI />
+            <CheckboxMUI />
+            <SwichMUI />
+            <RatingMUI />
+            <AutoCompleteMUI />
+            <LayoutMUI />
+            <CardMUI />
+            <AccordianMUI />
+            <MUIresponsiveness />
+            <TableMUI />
+            <ListMUI />
+          </Stack> */}
+        </ThemeProvider>
+      </QueryClientProvider>
     </>
   );
 }
---
+++ src/components/paginationMUI.tsx
@@ -0,0 +1,107 @@
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Stack,
+  Typography,
+  Pagination,
+} from "@mui/material";
+import axios from "axios";
+import { useQuery } from "@tanstack/react-query";
+import { useState } from "react";
+
+interface DataType {
+  name: string;
+  url: string;
+}
+
+export const PaginationMUI = () => {
+
+  const [pageNo, setPageNo] = useState(1);
+
+  // react query loginc
+  const { data, isLoading, isError } = useQuery(
+    ["paginationData", pageNo],
+    async () => {
+      try {
+        const response = await axios.get(
+          `https://pokeapi.co/api/v2/pokemon?limit=10&offset=${pageNo}`
+        );
+        return response.data;
+      } catch (error) {
+        throw new Error("Failed to fetch data");
+      }
+    }
+  );
+
+  const getPokemonId = (url: string) => {
+    const regex = /\/pokemon\/(\d+)\//;
+    const match = url.match(regex);
+    if (match) {
+      return match[1];
+    }
+    return "";
+  };
+
+  const handleChangePage = (
+    event: React.ChangeEvent<unknown>,
+    value: number
+  ) => {
+    setPageNo((prevPageNo) => (value > prevPageNo ? value : prevPageNo - 1));
+  };
+
+  if (isLoading) {
+    return <Typography variant="h1">Loading....</Typography>;
+  }
+  if (isError) {
+    return <Typography variant="h1">Error...</Typography>;
+  }
+
+  return (
+    <Stack alignItems={"center"} justifyContent={"space-evenly"} gap={4}>
+      <Box
+        display={"flex"}
+        gap={5}
+        flexDirection={"row"}
+        flexWrap={"wrap"}
+        justifyContent={"space-between"}
+        width={"80%"}
+      >
+        {data.results.map((items: DataType) => {
+          return (
+            <Card sx={{ width: "30%" }} key={items.name}>
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  height="100%"
+                  width='100%'
+                  image={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${getPokemonId(
+                    items.url
+                  )}.png`}
+                  alt="image"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    {items.name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Card>
+          );
+        })}
+      </Box>
+
+      <Box padding={4}>
+        <Pagination
+          count={6}
+          page={pageNo}
+          color="primary"
+          size="large"
+          onChange={handleChangePage}
+        />
+      </Box>
+    </Stack>
+  );
+};

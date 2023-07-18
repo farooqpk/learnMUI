46f6f223592812950aa278e73612de99ec915944
Ummar Farooq <farooqcherur@gmail.com>
2023-07-18 16:12:16 +0530

    theme customized

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Stack } from "@mui/material";
+import { Stack, colors, createTheme, ThemeProvider } from "@mui/material";
 import { ButtonMUI } from "./components/buttonMUI";
 import { TypographyMUI } from "./components/typographyMUI";
 import { TextFieldMUI } from "./components/textFieldMUI";
@@ -13,24 +13,37 @@ import { CardMUI } from "./components/cardMUI";
 import { AccordianMUI } from "./components/accordianMUI";
 import { MUIresponsiveness } from "./components/MUIresponsiveness";
 
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: colors.deepOrange[500],
+    },
+    secondary: {
+      main: colors.cyan[500],
+    },
+  },
+});
+
 function App() {
   return (
     <>
-      <Stack gap={7}>
-        <TypographyMUI />
-        <ButtonMUI />
-        <TextFieldMUI />
-        <SelectMUI />
-        <RadioMUI />
-        <CheckboxMUI />
-        <SwichMUI />
-        <RatingMUI />
-        <AutoCompleteMUI />
-        <LayoutMUI/>
-        <CardMUI/>
-        <AccordianMUI/>
-        <MUIresponsiveness/>
-      </Stack>
+      <ThemeProvider theme={theme}>
+        <Stack gap={7}>
+          <TypographyMUI />
+          <ButtonMUI />
+          <TextFieldMUI />
+          <SelectMUI />
+          <RadioMUI />
+          <CheckboxMUI />
+          <SwichMUI />
+          <RatingMUI />
+          <AutoCompleteMUI />
+          <LayoutMUI />
+          <CardMUI />
+          <AccordianMUI />
+          <MUIresponsiveness />
+        </Stack>
+      </ThemeProvider>
     </>
   );
 }
